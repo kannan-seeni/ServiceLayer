ed3008385fe1003e7c0e7e708896a10d112106db
kannan <kannansmart973@gmail.com>
2024-08-13 14:39:28 +0530

    enums

--- TNCSC.Hulling.Domain/Enums.cs
+++ TNCSC.Hulling.Domain/Enums.cs
@@ -14,5 +14,34 @@
             All = 0
         }
         #endregion
+
+        #region GunnyCondition
+        public enum GunnyCondition
+        {
+            NB = 1,
+            ONB = 2,
+            SS = 3,
+            SWP = 4,
+            US = 5
+        }
+        #endregion
+
+        #region Variety
+        public enum Variety
+        {
+            ADT = 1,
+            CR = 2
+        }
+        #endregion
+
+        #region Grade
+        public enum Grade
+        {
+            ADT36,
+            ADT37,
+            CR1006,
+            CR1007
+        }
+        #endregion
     }
 }
